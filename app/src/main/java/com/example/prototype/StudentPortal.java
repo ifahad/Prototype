798f4547dc3fe5c2947738852d7cef8481850e6b
qshaden <im3gax@gmail.com>
2017-04-26 08:02:04 +0300

    In develpment ..

--- app/src/main/java/com/example/prototype/StudentPortal.java
+++ app/src/main/java/com/example/prototype/StudentPortal.java
@@ -1,19 +1,13 @@
 package com.example.prototype;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class NewRequest extends AppCompatActivity {
+public class StudentPortal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_request);
+        setContentView(R.layout.activity_student_portal);
     }
-
-
 }
-
